1f3186f91e2b958849afbba4100db8f897dd9e96
tsunat973 <t23am018@yamanashi.ac.jp>
2026-07-24 13:29:10 +0900

    データの永続化(SQLite)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { DatabaseSync } = require("node:sqlite");
 
 app.get("/", (req, res) => {
     res.send("Hello");
@@ -15,19 +16,33 @@ app.use(express.json()); //POSTでJSONを受け取れるようにする
 
 let todos = [];
 
-app.get("/todos", (req, res) => {
-    res.json(todos);
+//データベース
+const db = new DatabaseSync("todos.db");
 
+db.exec(`
+    CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    text TEXT NOT NULL,
+    completed INTEGER NOT NULL DEFAULT 0
+    )
+    `);
+
+app.get("/todos", (req, res) => {
+    const todos = db.prepare("SELECT * FROM todos").all();
+    res.json(todos);
 });
 app.post("/todos", (req, res) => {
-    todos = [...todos, { text: req.body.text, completed: false }];
+    db.prepare("INSERT INTO todos(text, completed) VALUES (?, ?)").run(req.body.text, 0);
+    const todos = db.prepare("SELECT * FROM todos").all();
     res.json(todos);
 });
 
 
 app.delete("/todos/:id", (req, res) => {
     const id = Number(req.params.id);
-    todos = todos.filter((todo, index) => index !== id);
+    db.prepare("DELETE FROM todos WHERE id = ?").run(id);
+
+    const todos = db.prepare("SELECT * FROM todos").all();
     res.json(todos);
 });
 
@@ -35,14 +50,13 @@ app.delete("/todos/:id", (req, res) => {
 //map() は、配列の各要素に対して処理を行い、その return の値を集めて新しい配列を作るメソッドです。
 app.put("/todos/:id", (req, res) => {
     const id = Number(req.params.id);
-    todos = todos.map((todo, index) => {
-        if (index === id) {
-            return {
-                ...todo,
-                ...req.body,
-            };
-        }
-        return todo;
-    })
+    //今のtodoを取得
+    const current = db.prepare('SELECT * FROM todos WHERE id = ?').get(id);
+    //req.bodyにあるものだけ上書き、なければ今の値のまま
+    const text = req.body.text ?? current.text;
+    const completed = req.body.completed !== undefined ? (req.body.completed ? 1 : 0) : current.completed;
+
+    db.prepare("UPDATE todos SET text = ?, completed = ? WHERE id = ?").run(text, completed, id);
+    const todos = db.prepare("SELECT * FROM todos").all();
     res.json(todos);
 });
